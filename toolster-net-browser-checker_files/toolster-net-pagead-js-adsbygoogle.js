407f1d3c81c9aba7da19974abf58a52ef8b8a9b6
andrew.dragunskih <andrew.dragunskih@yandex.ru>
2022-09-05 23:33:54 +0500

    add default dir

--- toolster-net-browser-checker_files/toolster-net-pagead-js-adsbygoogle.js
+++
@@ -1,8 +0,0 @@
-<div style="width: 100%; height: 100%;">
-    <div style="width: 600px; margin: 0 auto; margin-top: 100px; font-size:50;">
-
-        <span>Страница не найдена (404)</span>
-        <img src="/images/system/404.jpg" alt="Страница не найдена (404)."/>
-
-    </div>
-</div>
